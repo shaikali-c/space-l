eeb0c2c75134afcaaa2593c1a098510bdf4fdb7d
Bubu <shaikmarwaan961@gmail.com>
2025-12-13 15:09:07 +0530

    A good start with a nice & clean homepage

--- app/page.js
+++ app/page.js
@@ -1,65 +1,45 @@
 import Image from "next/image";
+import SpaceImage from "@/public/assets/images/home.jpg";
+import Link from "next/link";
+import { ArrowUpRight, ChevronsRight, Search } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main className="grid grid-cols-1 md:grid-cols-[60vw_40vw] min-h-screen">
+      <section className="relative">
+        <header className="md:px-20 md:py-15 p-7 pt-0 text-lg flex justify-between items-start">
+          <h2>SPACE.</h2>
+          <div className="bg-neutral-900 p-3 rounded-full">
+            <Search size={20} />
+          </div>
+        </header>
+        <div className="md:absolute bottom-5 left-5 md:p-20 p-7 pb-0 flex flex-col gap-4">
+          <h2 className="text-4xl max-w-150">
+            We were never meant to fit under one sky, we were meant to build our
+            own among the stars.
+          </h2>
+          <p className="max-w-80 text-neutral-400">
+            Share your space posts. Explore others. Interact, comment, and
+            connect with people who share your interests.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            href={"/"}
+            className="bg-neutral-900 px-6 rounded-md py-3 w-fit flex items-center gap-1.5 hover:bg-neutral-100 hover:text-neutral-950"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Explore
+            <ArrowUpRight size={20} />
+          </Link>
         </div>
-      </main>
-    </div>
+      </section>
+      <section className="">
+        <Image
+          src={"/assets/images/home.jpg"}
+          width={5780}
+          height={5047}
+          alt="Space Image"
+          className="h-full w-full object-cover"
+        />
+      </section>
+    </main>
   );
 }
